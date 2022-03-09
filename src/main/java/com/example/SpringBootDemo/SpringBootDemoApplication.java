08ee668e0b0c2d9036b543e8f9ac36425278e9a2
Matt <matthewastro@gmail.com>
2022-03-09 12:38:23 -0500

    Created models branch and added the student class

--- src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.SpringBootDemo;
 
+import com.example.SpringBootDemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +19,17 @@ public class SpringBootDemoApplication {
 	}
 
 	@GetMapping("/")
-	public String hello(){
-		return "Hello World!";
+	public List<Student> student()
+	{
+		return List.of(
+				new Student(
+						1L,
+						"Matt Ward",
+						24,
+						LocalDate.of(1997, Month.OCTOBER, 5),
+						"matthew@matthewsemail.com"
+
+				)
+		);
 	}
 }
