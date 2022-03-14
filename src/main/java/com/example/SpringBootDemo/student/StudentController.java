b9ab062353591d7ef74f10367693b1efb54077d5
Matt <matthewastro@gmail.com>
2022-03-14 15:27:48 -0400

    added postmapping to enable a better ux

--- src/main/java/com/example/SpringBootDemo/student/StudentController.java
+++ src/main/java/com/example/SpringBootDemo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.SpringBootDemo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +16,13 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping()
+    @GetMapping
     public List<Student> getStudent()
     {
         return studentService.getStudent();
-}}
+}
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+}
